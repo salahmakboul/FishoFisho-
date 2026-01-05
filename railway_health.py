f83bacfd3dcfb4dc5c28d5a0210fb4d751eeacac
salahmakboul <iuogh71@gmail.com>
2026-01-05 16:34:07 +0000

    Add health endpoints for Railway

--- railway_health.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.urls import path
-
-def railway_health(request):
-    return HttpResponse("Railway Health Check: OK", status=200)
-
-urlpatterns = [
-    path('railway-health/', railway_health),
-]
